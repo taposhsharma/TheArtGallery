b68501fdf7aa2f279d686b15386868f7676cbf7f
Taposh <taposhchandrasharma@gmail.com>
2023-07-18 11:57:42 +0530

    updated post

---
+++ client/src/components/UpdateProfile.vue
@@ -0,0 +1,138 @@
+<template>
+    <div>
+      <form @submit.prevent="submitForm">
+        <div class="mb-3">
+          <label for="name" class="form-label">Name</label>
+          <input type="text" id="name" v-model="formData.name" class="form-control">
+        </div>
+        <div class="mb-3">
+          <label for="about" class="form-label">About</label>
+          <textarea id="about" v-model="formData.about" class="form-control"></textarea>
+        </div>
+        <div class="mb-3">
+          <label class="form-label">Achievements</label>
+          <div v-for="index in achievementCounter" :key="index" class="input-group mb-3">
+          <input type="text" v-model="formData.achievements[index-1]" class="form-control">
+          <button type="button" class="btn btn-outline-danger" @click="removeAchievement(index-1)">Remove</button>
+        </div>
+        <button type="button" class="btn btn-outline-primary" @click="addAchievement">Add Achievement</button>
+      </div>
+      <div class="mb-3">
+        <label class="form-label">Images</label>
+        <div v-for="index in imageCounter" :key="index" class="input-group mb-3">
+          <input type="file" @change="handleImageUpload($event, index-1)" class="form-control-file" required>
+          <input type="text" v-model="formData.imageNames[index-1]" class="form-control mt-2" placeholder="Image Name" required>
+          <input type="text" v-model="formData.aboutImages[index-1]" class="form-control mt-2" placeholder="About Image" required>
+          <button type="button" class="btn btn-outline-danger" @click="removeImage(index-1)">Remove</button>
+        </div>
+        <button type="button" class="btn btn-outline-primary" @click="addImage">Add Image</button>
+      </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+      </form>
+    </div>
+  </template>
+  
+  <script>
+ import axios from "axios";
+  export default {
+    name:'CreateProfile',
+    data() {
+      
+      return {
+        id:'',
+        formData: {
+          name: '',
+          about: '',
+          images: [],
+          achievements:[],
+        imageNames: [],
+        aboutImages: []
+      },
+      imageCounter: 0,
+        achievementCounter:0,
+        token:null
+      };
+    },
+    created(){
+        this.id=1
+        const user = JSON.parse(localStorage.getItem("user"));
+        if(user===null){
+            this.$router.push({name:"login"})
+        }else{
+            this.token = user.token
+      axios.defaults.headers.common["Authorization"] = this.token ;
+      
+        axios.get("http://localhost:5000/artist/updateProfile/"+this.id)
+        .then(response=>{
+            console.log(response.data)
+
+            if(response.data !=="authorized"){
+                console.log("hii")
+                
+            }
+        })
+        .catch(err=>{
+           
+            console.log(err.response.data)
+            this.$router.push({name:"home"})
+        })
+    }
+    },
+
+    methods: {
+        addAchievement() {
+      this.formData.achievements.push('');
+      console.log(this.achievementCounter)
+      this.achievementCounter++;
+    },
+    removeAchievement(index) {
+      this.formData.achievements.splice(index, 1);
+      this.achievementCounter--;
+    },
+    addImage() {
+      this.formData.images.push(null);
+      this.formData.imageNames.push('');
+      this.formData.aboutImages.push('');
+      this.imageCounter++;
+    },
+    removeImage(index) {
+      this.formData.images.splice(index, 1);
+      this.formData.imageNames.splice(index, 1);
+      this.formData.aboutImages.splice(index, 1);
+      this.imageCounter--;
+    },
+    handleImageUpload(event, index) {
+      const file = event.target.files[0];
+      this.formData.images[index] = file;
+    },
+      submitForm() {
+    
+        console.log(this.formData);
+        const data={
+            name: this.formData.name,
+          about:this.formData.about,
+        achievements:this.formData.achievements,
+        imageNames: this.formData.imageNames,
+        aboutImages: this.formData.aboutImages
+        }
+        const formData = new FormData();
+      for (let i = 0; i < this.formData.images.length; i++) {
+        formData.append("images", this.formData.images[i]);
+        
+      }
+      formData.append("data",JSON.stringify(data))
+      console.log(formData)
+      axios.defaults.headers.common["Authorization"] = this.token ;
+         axios.post("http://localhost:5000/artist/createprofile",formData)
+         .then(response=>{
+            console.log(response)
+         })
+         .catch(err=>{
+            console.log(err)
+         })
+
+      }
+    }
+  };
+  </script>
+  
--- server/routes/post.js
+++ server/routes/post.js
@@ -12,12 +12,12 @@ const upload = multer({dest: "uploads/",
       fieldSize: 50 * 1024 * 1024 // 10MB
     }
   });
-
-  router.post("/createPost", upload.array("images"), async (req, res) => {
+// cretae post for event details 
+  router.post("/createPost", upload.array("images[]"), async (req, res) => {
     const files = req.files;
     console.log(files)
     const {caption,email,phoneNumber,link} = JSON.parse(req.body.data);
-    const imageBuffer = null
+    let imageBuffer = null
     try {
       await client.query('BEGIN');
    
@@ -33,7 +33,7 @@ const upload = multer({dest: "uploads/",
       const postValues = [caption,link,imageBuffer,req.id,email,phoneNumber];
       client.query(insertQuery, postValues);
       client.query('COMMIT');
-      res.status(200).send("Profile Submitted");
+      res.status(200).send("Post Submitted");
     
      
     
@@ -42,26 +42,81 @@ const upload = multer({dest: "uploads/",
     } catch (err) {
       await client.query('ROLLBACK');
       console.error(err);
-      res.status(500).send("Error while submitting the profile");
+      res.status(500).send("Error while submitting the post");
     } 
   });
   
-
+// check for authorization (primary check point)
 router.get("/check",async ( req ,res)=>{
      return res.status(200).send("authorized")
 })
 
-router.get("/data", async (req,res)=>{
-  const query ='select * from artist_profile'
-  await client.query(query,(err,results)=>{
-    if(err){
-      console.log(err)
-    }
-    else{
-      res.send(results)
-      console.log(results)
-    }
-  })
+// router.get("/data", async (req,res)=>{
+//   const query ='select * from artist_profile'
+//   await client.query(query,(err,results)=>{
+//     if(err){
+//       console.log(err)
+//     }
+//     else{
+//       res.send(results)
+//       console.log(results)
+//     }
+//   })
+// })
+
+
+
+// get data for update the post for event 
+router.get("/updatepost/:id",async (req,res)=>{
+    const id = req.params.id
+    // console.log(id)
+    const query = `select * from post where id = ${id}`
+    await client.query(query,(err,result)=>{
+        if(err){
+            console.log(err)
+        }
+        else{
+            // console.log(result.rows)
+            res.status(200).send(result.rows)
+        }
+    })
 })
 
+
+
+// post request to update the data in the post table 
+
+router.post('/updatepost' , upload.array("images[]"), async (req,res)=>{
+    const files = req.files;
+    console.log(files)
+    const {id,caption,email,phoneNumber,link} = JSON.parse(req.body.data);
+    let imageBuffer = null
+    try {
+        await client.query('BEGIN');
+     
+       if(files.length>0){
+    
+        for (let i = 0; i < files.length; i++) {
+          const file = files[0];
+          imageBuffer = fs.readFileSync(file.path);
+          const insertQuery = 'UPDATE post SET caption = $1, link = $2,image = $3,c_id = $4,email = $5, phone_no = $6 WHERE id = $7;';
+          const postValues = [caption,link,imageBuffer,req.id,email,phoneNumber,id];
+          client.query(insertQuery, postValues);
+          client.query('COMMIT');
+          res.status(200).send("Post Submitted");
+          
+        }}
+    else{
+        const insertQuery = 'UPDATE post SET caption = $1, link = $2,c_id = $3,email = $4, phone_no = $5 WHERE id = $6;';
+          const postValues = [caption,link,req.id,email,phoneNumber,id];
+          client.query(insertQuery, postValues);
+          client.query('COMMIT');
+          res.status(200).send("Post Submitted");
+    }}
+        catch(err) {
+            await client.query('ROLLBACK');
+            console.error(err);
+            res.status(500).send("Error while updating the post");
+          } 
+})
 module.exports = router
